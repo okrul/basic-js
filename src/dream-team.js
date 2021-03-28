d9b337ece6bbd5549451bf954a2fe6ae5e0f6fa2
Aliaksandr Okrul <okrul@list.ru>
2021-03-28 12:00:06 +0300

    feat: implement deam-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let name = '';
+  members.forEach(element => {
+    if (typeof element == 'string' && element.trim().length > 0) {
+      name = name + element.trim()[0].toUpperCase();
+    }
+  });
+
+  return name.length = 0?false:Array.from(name).sort().reduce((str, val) =>str + val, '');
+
 };
