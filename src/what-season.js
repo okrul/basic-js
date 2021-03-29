c0862cff2540fedd5e5339771f25b8b9836345c6
Aliaksandr Okrul <okrul@list.ru>
2021-03-30 00:05:03 +0300

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  //throw new CustomError('Not implemented');
+
+  if (date === undefined || !(date instanceof Date)) {
+     return 'Unable to determine the time of year!';
+  }
+
+  if (date === undefined) {
+    throw Error();
+  }
+
+  switch (date.getMonth()) {
+  case 0: return 'winter';
+  case 1: return 'winter';
+  case 2: return 'spring';
+  case 3: return 'spring';
+  case 4: return 'spring';
+  case 5: return 'summer';
+  case 6: return 'summer';
+  case 7: return 'summer';
+  case 8: return 'autumn';
+  case 9: return 'autumn';
+  case 10: return 'autumn';
+  case 11: return 'winter';
+  default: CustomError('THROWN');
+  }  
+
+  throw Error();
+
 };
